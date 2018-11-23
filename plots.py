b367e13e7f6c1ad28f508cf78dfd943ea9e704c6
VioletaLuna <32911004+VioletaLuna@users.noreply.github.com>
2018-11-23 07:35:01 -0500

    Creamos

---
+++ plots.py
@@ -0,0 +1,12 @@
+#Este archivo usa ecoding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt("walk.dat", delimiter = " ", skip_header = 0)
+datano = np.genfromtxt("walkno.dat", delimiter = " ", skip_header = 0)
+#fs
+
+plt.hist(data)
+plt.hist(datano)
+
+plt.savefig("walk.pdf")
